69e2150174b908a502a795e41ef47f1d2791d3aa
Faisal Afriansyah <faisalafriansyah08@gmail.com>
2026-06-14 23:02:46 +0700

    force redeploy

--- app/main.py
+++ app/main.py
@@ -59,4 +59,4 @@ if __name__ == "__main__":
     import os
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port) 

cfb3ace95801983cffb38c453ae3a0d8b76b5e18
Faisal Afriansyah <faisalafriansyah08@gmail.com>
2026-06-14 22:51:31 +0700

    fix CORS for netlify

--- app/main.py
+++ app/main.py
@@ -24,7 +24,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://dndra-room.netlify.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
